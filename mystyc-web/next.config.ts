9f4f53cca86125ed339e0cdb8120760517060e81
DennisHoskins <dennis.r.hoskins@gmail.com>
2025-06-14 15:50:47 -0600

    removed invalid config

--- mystyc-web/next.config.ts
+++ mystyc-web/next.config.ts
@@ -1,17 +1,15 @@
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    allowedDevOrigins: ['https://mystyc.app', 'https://www.mystyc.app']
-  },
-  async redirects() {
-    return [
-      {
-        source: '/__/auth/action',
-        destination: '/action',
-        permanent: true,
-      },
-    ];
-  },
+ reactStrictMode: false,
+ allowedDevOrigins: ['mystyc.app', 'www.mystyc.app', 'mystyc-client.loca.lt'],
+ async redirects() {
+   return [
+     {
+       source: '/__/auth/action',
+       destination: '/action',
+       permanent: true,
+     },
+   ];
+ },
 };
 
 module.exports = nextConfig;
